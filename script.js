9fff58bfe2b1b24914e673777f5546eed58ef0c0
moonjunsain <moonjunsain@gmail.com>
2023-06-28 15:34:50 -0700

    Added some console logs to make sure the logic works as intended and fixed the logic (parseInt before putting it in if statement)

--- script.js
+++ script.js
@@ -18,9 +18,12 @@ function generatePassword() {
   var passwordForReturn = "";
   var pLength = 0;
   pLength = prompt("How long do you want your password to be? (min: 8, max: 128)");
+  pLength = parseInt(pLength);
+ 
 
   // checks if pLength is actually an integer and within the range
   if(Number.isInteger(pLength) && pLength <= 128 && pLength >= 8){
+    console.log("The number is valid");
     // proceed
     // confirm for any necessary properties for the password
     var includeSpecial = confirm("Do you want to include special characters?");
@@ -31,6 +34,7 @@ function generatePassword() {
     
   } else {
     // pLength has to be an int
+    console.log("The number is not valid");
     return "Please Enter Proper Value";
   }
   
